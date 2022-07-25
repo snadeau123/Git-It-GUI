55de2613c61b165eb833c542b6129295240cda5e
Sebastien Nadeau <snadeau@breakingwalls.co>
2022-07-25 19:19:38 -0400

    added history and submodules basic functionality

---
+++ GitCommander/Repository_History.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace GitCommander
+{
+	public class HistoryState
+	{
+		public string name {get; internal set;}
+		public string url {get; internal set;}
+
+		public HistoryState() {}
+		public HistoryState(string name)
+		{
+			this.name = name;
+		}
+
+		public override string ToString()
+		{
+			return name;
+		}
+	}
+
+	public partial class Repository
+	{
+		public bool GetRecentHistory(out string history)
+		{
+			lock (this)
+			{
+				int length = 40;
+				var result = RunExe("git", string.Format("log --pretty=oneline --abbrev-commit --graph --decorate -n {0}", length));
+				lastResult = result.output;
+				lastError = result.errors;
+
+				if (!string.IsNullOrEmpty(lastError) || string.IsNullOrEmpty(lastResult))
+				{
+					history = null;
+					return false;
+				}
+
+				history = lastResult;
+				return true;
+			}
+		}
+
+	}
+}
---
+++ GitCommander/Repository_Submodules.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GitCommander
+{
+	public class SubmoduleState
+	{
+		public string name {get; internal set;}
+		public bool initialized {get; internal set;}
+		public bool synced {get; internal set;}
+
+		public SubmoduleState() {}
+		public SubmoduleState(string name)
+		{
+			this.name = name;
+			this.initialized = false;
+			this.synced = false;
+		}
+
+		public override string ToString()
+		{
+			return name;
+		}
+	}
+
+	public partial class Repository
+	{
+		public void GetSubmoduleList(out SubmoduleState[] submodules)
+		{
+			var states = new List<SubmoduleState>();
+
+			void stdCallback_Submodules(string line)
+			{
+				var match = Regex.Match(line, @"(\s|\-|\+)\w*\s(.*)\s\(.*\)");
+				if (match.Success)
+				{
+					SubmoduleState submodule = new SubmoduleState(match.Groups[2].Value);
+					switch (match.Groups[1].Value)
+					{
+						case " ":
+							submodule.synced = true;
+							submodule.initialized = true;
+							break;
+
+						case "-":
+							break;
+
+						case "+":
+							submodule.synced = false;
+							submodule.initialized = true;
+							break;
+
+						default:
+							break;
+					}
+					states.Add(submodule);
+				}
+			}
+
+			lock (this)
+			{
+				// check for submodules
+				var result = RunExe("git", "submodule status", stdCallback:stdCallback_Submodules);
+				lastResult = result.output;
+				lastError = result.errors;
+				
+				submodules = states.ToArray();
+			}
+		}
+	}
+}
